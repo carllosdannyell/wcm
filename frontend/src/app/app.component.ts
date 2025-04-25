99e30d9460f3b7dade17285f2b66c1dd7093e220
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2025-04-25 16:34:18 -0300

    corrige erro de rota

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,8 +14,18 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    if (typeof window !== 'undefined' && !localStorage.getItem('access_token')) {
-      this.router.navigate(['login']);
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem('access_token');
+      const currentUrl = this.router.url;
+  
+      const hasRedirected = sessionStorage.getItem('has_redirected');
+  
+      if (!token) {
+        this.router.navigate(['login']);
+      } else if ((currentUrl === '/' || currentUrl === '') && !hasRedirected) {
+        sessionStorage.setItem('has_redirected', 'true');
+        this.router.navigate(['/dashboard/panel']);
+      }
     }
   }
 }
